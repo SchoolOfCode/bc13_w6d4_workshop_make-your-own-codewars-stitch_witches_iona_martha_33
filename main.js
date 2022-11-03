447f6cd6bb0b01eb25e095ab1c42ee6704e4db39
marthamaybennett <marthamaybennett123@gmail.com>
2022-11-03 16:08:50 +0000

    We did itgit add .!

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 /* 
 👉 Welcome to Spin Us A Yarn, a knit-it-yourself workshop! Select your size and design to work out 
 how many balls of wool you will need to make your jumper.
-However, you already have some of the colours in your wool stash, namely: 100g of black, and 200g of periwinkle.
 
 Sizes: S = 600g, M = 800g, L 1kg, XL 1.2kg
 
@@ -11,10 +10,9 @@ This means that you will have to buy a whole ball of a colour even if you only n
 
 Write a function to determine how many balls of wool you will need in total for a specified design and size.
 
-Cat Design: [10% black, 40% periwinkle, 40% orange, 5% pink, 5% white]
-Rabbit Design: [40% white, 10% periwinkle, 15% pink, 10% black, 25% green]
-Fox Design: [10% black, 45% red, 10% pink, 35% green]
-
+Cat Design: [10% black, 40% periwinkle, 40% orange, 5% white, 5% green]
+Rabbit Design: [10% black, 10% periwinkle, 0% orange, 40% white, 40% green]
+Fox Design: [10% black, 35% periwinkle, 45% orange, 5% white, 5% green]
 
 800 total
 black = 80g, umber = 320, red = 40g, pink = 80g, green = 280
@@ -23,18 +21,68 @@ total wool balls: 7
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
-function ballsOfWool (size, design){
+
+let weight = 0
+
+export default function ballsOfWool (size, design){
 if (size === "S"){
-    let weight = 600
+    weight = 600
 } 
 else if (size === "M"){
-    let weight = 800
+    weight = 800
 } 
 else if (size === "L"){
-    let weight = 1000
+    weight = 1000
 } 
 else if (size === "XL"){
-    let weight = 1200
+    weight = 1200
 } 
-let cat =
+
+let woolCount = 0
+
+if (design === "cat") {
+    let catDesign = [10, 40, 40, 5, 5]
+    for ( let i = 0; i < catDesign.length; i++) {
+        let percentage = (weight/100)
+        let woolWeight = percentage * catDesign[i]
+        if (woolWeight <= 200 && woolWeight >0) {
+         woolCount++
+        }
+        else if (woolWeight > 200 && woolWeight <= 400) {
+         woolCount = woolCount + 2
+        }
+        else if (woolWeight > 400) {
+         woolCount = woolCount + 3
+        }
+     }
 }
+else if (design === "rabbit") {
+    let rabbitDesign = [10, 10, 0, 40, 40]
+    for ( let i = 0; i < rabbitDesign.length; i++) {
+        let percentage = (weight/100)
+        let woolWeight = percentage * rabbitDesign[i]
+        if (woolWeight <= 200 && woolWeight >0) {
+         woolCount++
+        }
+        else if (woolWeight > 200 && woolWeight <= 400) {
+         woolCount = woolCount + 2
+        }
+        else if (woolWeight > 400) {
+        woolCount = woolCount + 3}}}
+
+else if (design === "fox") {
+    let foxDesign = [10, 35, 45, 5, 5]
+    for ( let i = 0; i < foxDesign.length; i++) {
+            let percentage = (weight/100)
+            let woolWeight = percentage * foxDesign[i]
+            if (woolWeight <= 200 && woolWeight >0) {
+            woolCount++
+            }
+            else if (woolWeight > 200 && woolWeight <= 400) {
+            woolCount = woolCount + 2
+            }
+            else if (woolWeight > 400) {
+            woolCount = woolCount + 3}}}
+        
+return woolCount}
+    
--- main.test.js
+++ main.test.js
@@ -1,1 +1,15 @@
 //👉 Write your tests below here:
+import ballsOfWool from "./main.js"
+
+test("large fox", () => {
+    expect(ballsOfWool("L", "fox")).toBe(8);
+});
+test("small rabbit", () => {
+    expect(ballsOfWool("S", "rabbit")).toBe(6);
+});
+test("XL cat", () => {
+    expect(ballsOfWool("XL", "cat")).toBe(9);
+});
+test("M rabbit", () => {
+    expect(ballsOfWool("M", "rabbit")).toBe(6);
+});
