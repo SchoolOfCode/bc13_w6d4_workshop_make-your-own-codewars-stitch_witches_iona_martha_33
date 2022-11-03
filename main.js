4bf7f020e4c36d1b3280e4091366eafe54f32145
ionarose <ionaroseuk@gmail.com>
2022-11-03 15:03:04 +0000

    problem written

--- main.js
+++ main.js
@@ -1,5 +1,40 @@
 /* 
-👉 Write your kata here!
+👉 Welcome to Spin Us A Yarn, a knit-it-yourself workshop! Select your size and design to work out 
+how many balls of wool you will need to make your jumper.
+However, you already have some of the colours in your wool stash, namely: 100g of black, and 200g of periwinkle.
+
+Sizes: S = 600g, M = 800g, L 1kg, XL 1.2kg
+
+1 ball of wool weighs 200g.
+
+This means that you will have to buy a whole ball of a colour even if you only need 20g worth.
+
+Write a function to determine how many balls of wool you will need in total for a specified design and size.
+
+Cat Design: [10% black, 40% periwinkle, 40% orange, 5% pink, 5% white]
+Rabbit Design: [40% white, 10% periwinkle, 15% pink, 10% black, 25% green]
+Fox Design: [10% black, 45% red, 10% pink, 35% green]
+
+
+800 total
+black = 80g, umber = 320, red = 40g, pink = 80g, green = 280
+black = 1, umber = 2, red = 1, pink = 1, green = 2
+total wool balls: 7
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
+function ballsOfWool (size, design){
+if (size === "S"){
+    let weight = 600
+} 
+else if (size === "M"){
+    let weight = 800
+} 
+else if (size === "L"){
+    let weight = 1000
+} 
+else if (size === "XL"){
+    let weight = 1200
+} 
+
+}
